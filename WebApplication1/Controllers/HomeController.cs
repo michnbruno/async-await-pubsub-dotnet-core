68a4abee4815020b981778a572a94b33cddc2334
Michael Bruno <michnbruno@gmail.com>
2023-03-08 10:08:08 -0500

    update configuration

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,26 +8,52 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            // ViewBag.Message = "have a nice day";
+            // ViewBag.ConnectionString = _configuration["pubsub-connection"];
+            //// ViewBag.ConnectionString = "Endpoint=https://pubsub-testmnb.webpubsub.azure.com;AccessKey=OhTvi1hB1NKftf9TtEgBlkC/+PXHu2HiTrmlkg3nTq4=;Version=1.0;";
+            // ViewBag.Hub = "myHub1";
+            // ViewBag.EmittedMessages = "start_";
+
+            // var pubSubClient = new WebPubSubServiceClient(
+            //         ViewBag.ConnectionString, // ViewBag.ConnectionString,
+            //         ViewBag.Hub);
+
+            // var url = pubSubClient.GetClientAccessUri();
+            // ViewBag.ClientConnection = url;
+
+            return View("About");
+        }
+
+        public IActionResult Pubsub()
+        {
             ViewBag.Message = "have a nice day";
-            ViewBag.ConnectionString = "Endpoint=https://pubsub-testmnb.webpubsub.azure.com;AccessKey=OhTvi1hB1NKftf9TtEgBlkC/+PXHu2HiTrmlkg3nTq4=;Version=1.0;";
+            ViewBag.ConnectionString = _configuration["pubsub-connection"];
+            // ViewBag.ConnectionString = "Endpoint=https://pubsub-testmnb.webpubsub.azure.com;AccessKey=OhTvi1hB1NKftf9TtEgBlkC/+PXHu2HiTrmlkg3nTq4=;Version=1.0;";
             ViewBag.Hub = "myHub1";
             ViewBag.EmittedMessages = "start_";
 
             var pubSubClient = new WebPubSubServiceClient(
-                    ViewBag.ConnectionString,
+                    ViewBag.ConnectionString, // ViewBag.ConnectionString,
                     ViewBag.Hub);
 
             var url = pubSubClient.GetClientAccessUri();
             ViewBag.ClientConnection = url;
 
+            return View();
+        }
+
+        public IActionResult About()
+        {
             return View();
         }
 
